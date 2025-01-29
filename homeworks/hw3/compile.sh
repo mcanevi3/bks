533497ca18f58676a492b8fef0ce8ffe6a6a0d22
Mehmet Canevi <m.canevi3@gmail.com>
2025-01-29 17:51:54 +0300

    questions added

---
+++ homeworks/hw3/compile.sh
@@ -0,0 +1,14 @@
+pdflatex --shell-escape main
+pdflatex --shell-escape main
+pdflatex --shell-escape main2
+pdflatex --shell-escape main2
+
+rm *.gnuplot
+rm *.table
+rm *.aux
+rm *.log
+rm *.vrb
+rm *.nav
+rm *.out
+rm *.snm
+rm *.toc
