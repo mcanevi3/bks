7a78d589b1597d349428f5635a776a5713c61d26
Mehmet Canevi <m.canevi3@gmail.com>
2025-01-31 08:57:21 +0300

    lec5

--- lecture notes/python/lec5/run1.py
+++ lecture notes/python/lec5/run1.py
@@ -8,8 +8,36 @@ import control
 ############################
 def fun_q1():
     T=0.2
-    den=[1 0.3 4.22 -3.58 -0.4]
-    poles=roots(den)
-    Gz=tf(1,den,T)
+    Gz=control.tf(1,np.array([1,0.3,4.22,-3.58,-0.4]),T)
+    control.rlocus(Gz)
+    plt.show()
+############################
+# q2
+############################
+def fun_q2():
+    z=sym.Symbol('z')
+    k=sym.Symbol('k')
+    pol=sym.Poly(z**4+0.3*z**3+4.22*z**2-0.4-3.58*k*z+2*k,k)
+    coef=pol.coeffs()
+    num=sym.Poly(coef[0],z).all_coeffs()
+    den=sym.Poly(coef[1],z).all_coeffs()
+    num=np.array(num,dtype=np.float64)
+    den=np.array(den,dtype=np.float64)
 
-fun_q1()
+    # print(num)
+    # print(den)
+    Gz=control.tf(num,den)
+    control.rlocus(Gz)
+    plt.axis([-1.1,1.1,-1.1,1.1])
+    plt.show()
+############################
+# q3
+############################
+def fun_q3():
+    z=sym.Symbol('z')
+    k=sym.Symbol('k')
+    expr=(-3.58*z+2)/(z**4+0.3*z**3+4.22*z**2-0.4)
+    sol=sym.solve(sym.diff(expr,z),z)
+    print(sol)
+
+fun_q3()
