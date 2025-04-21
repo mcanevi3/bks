db65361e76c28d977ae5a9a80d68374261b2d137
Mehmet Canevi <m.canevi3@gmail.com>
2025-04-21 09:07:58 +0300

    lecture notes split

---
+++ lecture notes/splitt.bat
@@ -0,0 +1,15 @@
+@echo off
+pdftk main.pdf cat 1-4 output Kapak.pdf
+pdftk main.pdf cat 5-12 output Bolum1.pdf
+pdftk main.pdf cat 13-26 output Bolum2.pdf
+pdftk main.pdf cat 27-32 output Bolum3.pdf
+pdftk main.pdf cat 33-40 output Bolum4.pdf
+pdftk main.pdf cat 41-48 output Bolum5.pdf
+pdftk main.pdf cat 49-54 output Bolum6.pdf
+pdftk main.pdf cat 55-60 output Bolum7.pdf
+pdftk main.pdf cat 61-64 output Bolum8.pdf
+pdftk main.pdf cat 65-68 output Bolum9.pdf
+pdftk main.pdf cat 69-72 output Bolum10.pdf
+pdftk main.pdf cat 73-78 output Bolum11.pdf
+pdftk main.pdf cat 79-84 output Bolum12.pdf
+pdftk main.pdf cat 85-89 output Bolum13.pdf
