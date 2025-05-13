de9ea56a33a1a62dd1a179be7d60d86688f5e260
Mehmet Canevi <m.canevi3@gmail.com>
2025-05-13 11:32:16 +0300

    cont'd

--- final_problems/q1.py
+++ final_problems/q1.py
@@ -1,9 +1,12 @@
 from numpy import *
-from control import *
 from sympy import *
 from matplotlib.pyplot import *
+import control
 
-ts=1
+num_gs=1
+den_gs=[1,1,1]
+
+ts=0.5
 os=10/100
 
 zeta=-log(os)/sqrt(np.pi**2+log(os)**2)
@@ -18,7 +21,7 @@ kd=Symbol('kd')
 kp=Symbol('kp')
 ki=Symbol('ki')
 
-Gss=1/(s**2+s+1)
+Gss=num_gs/(den_gs[0]*s**2+den_gs[1]*s+den_gs[2])
 Fss=kd*s+kp+ki/s
 Tss=(Fss*Gss)/(1+Fss*Gss)
 Tss=simplify(Tss)
@@ -33,30 +36,66 @@ for i in range(len(coef_pcs)):
 sol=solve(prob,[kd,kp,ki])
 
 pzs2=pzs.subs(kd,sol[kd]).subs(kp,sol[kp]).subs(ki,sol[ki])
-
 temp=Poly(pzs2,p).all_coeffs()
 
 num=[float(x) for x in Poly(temp[0],s).all_coeffs()]
 den=[float(x) for x in Poly(temp[1],s).all_coeffs()]
-Gzs=tf(num,den)
-
-figure(1)
-clf()
-rlocus(Gzs)
-# show()
+Gzs=control.tf(num,den)
 
 pvec=np.arange(0.1,20,0.1)
+mzvec=np.zeros(pvec.shape)
+mpvec=np.zeros(pvec.shape)
 mvec=np.zeros(pvec.shape)
 fpzs=lambdify(p,Poly(pzs2,s).all_coeffs()) 
 
 for i,pval in enumerate(pvec):
     zeross=np.roots(fpzs(pval))
     mz=[np.abs(np.real(z))/(zeta*wn) for z in zeross]
+    mzvec[i]=min(mz)
     mp=pval/(zeta*wn)
-    mvec[i]=min([min(mz),mp])
+    mpvec[i]=mp
+    mvec[i]=min([mzvec[i],mpvec[i]])
 
 index=argmax(mvec)
 mval=mvec[index]
 pval=pvec[index]
 
-print(mval,pval)
+figure(1)
+clf()
+subplot(1,3,1)
+control.rlocus(Gzs)
+
+zeross=np.roots(fpzs(pval))
+for ztemp in zeross:
+    plot(real(ztemp),imag(ztemp),'kx')
+title(" ")
+
+
+subplot(1,3,2)
+plot(pvec,mpvec,'r',linewidth=2)
+plot(pvec,mzvec,'b',linewidth=2)
+plot(pvec,mvec,'m',linewidth=1)
+plot(pval,mval,'kx',linewidth=2)
+
+kdval=float(sol[kd].subs(p,pval))
+kpval=float(sol[kp].subs(p,pval))
+kival=float(sol[ki].subs(p,pval))
+
+Fs=control.tf([kdval,kpval,kival],[1,0])
+Gs=control.tf(num_gs,den_gs)
+Ts=control.feedback(Fs*Gs,1)
+print(Ts)
+
+cpoles=[complex(x )for x in control.poles(Ts)]
+czeros=[complex(x )for x in control.zeros(Ts)]
+print(cpoles)
+print(czeros)
+
+subplot(1,3,3)
+t,y=control.step_response(Ts)
+plot(t,y,'k',linewidth=2)
+info=control.step_info(Ts)
+tsval=np.round(info['SettlingTime'],2)
+osval=np.round(info['Overshoot'],2)
+title(f"ts:{tsval} s os:%{osval}")
+show()
