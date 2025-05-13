c672e6d2f504961e136caad749265ac51f8e9b1f
Mehmet Canevi <m.canevi3@gmail.com>
2025-05-13 19:09:18 +0300

    final exam rework

--- exams/final2/run2.m
+++ exams/final2/run2.m
@@ -1,28 +1,36 @@
 clear;clc;
-T=1;
-A=[0,1;-1,-1];
-B=[0;1];
-C=[1,0];
 
-z=sym('z');
-pdz=z^2+0.5*z+0.25;
-pdA=A*A+0.5*A+0.25*eye(2);
-disp("pdA:");
-disp(pdA);
+os=16.3/100;
+ts=2;
 
-phi=[B,A*B];
-disp("phi:");
-disp(phi);
+syms s;
+zeta=-log(os)/sqrt(pi^2+log(os)^2);
+wn=4/(ts*zeta);
+pds=s^2+2*zeta*wn*s+wn^2;
 
-K=-[0,1]*inv(phi)*pdA;
-disp("K:");
-disp(K);
+Gs=1/(s+0.2);
+syms ki kp real;
+Fs=kp+ki/s;
+Ts=(Fs*Gs)/(1+Fs*Gs);
+[pzs,pcs]=numden(Ts);
 
-disp("Some matrices:");
-disp("inv");disp(phi);disp("=");
-disp(inv(phi));
-disp("A*A:");
-disp(A*A);
+coef=coeffs(pcs,s,'all');
+coef=coef/coef(1);
+prob=coef==coeffs(pds,s,'all');
+for i=1:length(prob)
+    disp(prob(i))
+end
+sol=solve(prob);
+kpval=double(sol.kp);
+kival=double(sol.ki);
 
-lambda=eig(A+B*K);
-lambda
+disp("kp:"+string(kpval));
+disp("ki:"+string(kival));
+
+Fs=tf([kpval kival],[1 0]);
+[num,den]=numden(Gs);
+Gs=tf(double(coeffs(num,s,'all')),double(coeffs(den,s,'all')));
+Ts=feedback(Fs*Gs,1);
+Ts
+stepinfo(Ts)
+
